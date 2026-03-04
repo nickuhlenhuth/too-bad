9c7ee8aaa6a77b695ffe707aefe83e2c093aed21
Nick Uhlenhuth <uhlenhuthn@gmail.com>
2026-03-04 14:29:08 -0600

    faster refresh

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -6,7 +6,7 @@ export const client = createClient({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID || "placeholder",
   dataset: import.meta.env.PUBLIC_SANITY_DATASET || "production",
   apiVersion: "2024-01-01",
-  useCdn: true,
+  useCdn: false,
 });
 
 const builder = createImageUrlBuilder(client);
